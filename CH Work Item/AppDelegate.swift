2c39c00404a7cd0e08c6594e9c7ffb6aa2858b5d
Christopher Hannah <christopher@Wrathion.local>
2016-12-27 17:44:38 +0000

    Initial Commit

---
+++ CH Work Item/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CH Work Item
+//
+//  Created by Christopher Hannah on 27/12/2016.
+//  Copyright © 2016 Christopher Hannah. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
